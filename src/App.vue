3b781acafc875d6ba8607f5f93ba026e2f827d9d
vitorr7df <vitor.rochadf@gmail.com>
2025-06-22 17:57:40 -0300

    atualidando vite e styles css

--- src/App.vue
+++ src/App.vue
@@ -52,9 +52,9 @@ nextTick(() => {
 @import "vuetify/styles";
 @import "@mdi/font/css/materialdesignicons.css";
 @import "sweetalert2/src/sweetalert2.scss";
-@import "@/assets/index.scss";
+@import "@/assets/index.css";
 @import "animate.css";
-@import "@/styles/global.scss";
+@import "@/styles/global.css";
 
 #pnCalk {
   top: 260px;
