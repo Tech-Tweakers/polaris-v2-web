0fef6fb2cb770147fd517a0234ac8a8e5258cbc1
Andre Torres <andreh.torres@hotmail.com>
2025-07-13 10:43:17 -0300

    wip: new datacenter

---
+++ docs/assets/_...all_-BN6TA4QY.js
@@ -0,0 +1,1 @@
+import{_ as e,b as n,o}from"./index-C2Sp98hP.js";const _={};function c(r,t){return o(),n("h1",null,"rota não encontrada")}const s=e(_,[["render",c]]);export{s as default};
--- docs/assets/_...all_-Dh14uYvG.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as n,o}from"./index-CatWOvwC.js";const _={};function c(r,t){return o(),n("h1",null,"rota não encontrada")}const s=e(_,[["render",c]]);export{s as default};
