d0f47ce33bcfacd90faa14cf6bdcafa8d062d9ef
Your Name <you@example.com>
2025-09-02 12:09:39 -0300

    fix: remove #

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 //@ts-ignore
 import routes from "virtual:generated-pages";
 
 const router = createRouter({
-  history: createWebHistory('/polaris-v2-web/'),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
